b0a42fd688703b92b6a49b0a018a748206afa5e2
Lucas Araújo <lucasaraujodev.contact@gmail.com>
2024-04-06 16:11:01 -0300

    exercises #14 and #15 added
    explanation of exercises #14 and #15 included (arrays)

---
+++ 014-waiting_line/waiting-line.js
@@ -0,0 +1,22 @@
+// ----- EXPLANATION OF THE EXERCISE -----
+
+/*
+
+🇺🇸: Write a JavaScript program to simulate a waiting line at a doctor's office.
+
+The program must start by showing an interactive menu on the screen containing a list of all patients waiting in order, showing their position next to their name (e.g.: 1st Matheus, 2nd Marcos, etc.).
+
+The menu should also allow you to choose between the options “New patient”, to add a new patient to the end of the queue (asking for the patient's name), “Consult patient”, which removes the first patient from the queue and shows the name on the screen of the consulted patient, and “Exit”.
+
+The program must only be closed by choosing the “Exit” option, otherwise you must return to the menu.
+
+🇧🇷: Escreva um programa em javascript para simular uma fila de espera em um consultório médico. 
+
+O programa deve iniciar mostrando na tela um menu interativo contendo a lista de todos os pacientes esperando em ordem mostrando sua posição ao lado do nome (ex.: 1º Matheus, 2º Marcos, etc). 
+
+O menu também deve permitir escolher entre as opções de “Novo paciente”, para adicionar um novo paciente ao fim da fila (pedindo o nome do paciente), “Consultar paciente”, que retira o primeiro paciente da fila e mostra na tela o nome do paciente consultado, e “Sair”. 
+
+O programa só deve ser encerrado ao escolher a opção “Sair”, caso contrário deve voltar ao menu.
+
+*/
+
---
+++ 015-stack_of_cards/stack-of-cards.js
@@ -0,0 +1,22 @@
+// ----- EXPLANATION OF THE EXERCISE -----
+
+/*
+
+🇺🇸: Write a JavaScript program to simulate a deck of cards.
+
+The program should start by showing an interactive menu on the screen containing the number of cards currently in the deck and the options to “Add a card”, “Draw a card” and “Exit”.
+
+When choosing “Add a card”, the program should ask for the name of the card and add it to the top of the deck. When choosing “Pull a card”, the program must remove the card from the top of the deck and show the name of the card drawn on the screen.
+
+The program must only be closed by choosing the “Exit” option, otherwise you must return to the menu.
+
+🇧🇷: Escreva um programa em javascript para simular um baralho de cartas. 
+
+O programa deve iniciar mostrando na tela um menu interativo contendo a quantidade de cartas que estão atualmente no baralho e as opções de “Adicionar uma carta”, “Puxar uma carta” e “Sair”. 
+
+Ao escolher “Adicionar uma carta”, o programa deve perguntar o nome da carta e adicioná-la no topo do baralho. Ao escolher “Puxar uma carta”, o programa deve retirar a carta do topo do baralho e mostrar na tela o nome da carta puxada. 
+
+O programa só deve ser encerrado ao escolher a opção de “Sair”, caso contrário deve voltar ao menu.
+
+*/
+
