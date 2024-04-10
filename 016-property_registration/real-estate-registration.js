917131250c06030fa4a066969a86c30d57becd85
Lucas Araújo <lucasaraujodev.contact@gmail.com>
2024-04-10 20:17:03 -0300

    exercise #16
    included

---
+++ 016-property_registration/real-estate-registration.js
@@ -0,0 +1,29 @@
+// ----- EXPLANATION OF THE EXERCISE -----
+
+/*
+
+🇺🇸: Write a JavaScript program that works as a property registry and meets the following requirements:
+
+      1. It must have an interactive menu that is always displayed until the user's choice exits.
+      2. The interactive menu must show the number of registered properties at the top.
+      3. The menu must have the option to save a property.
+      4. To save a new property, the program must ask for the following information: Owner's name. Number of bedrooms. Number of bathrooms. If it has a garage.
+          4.1. Owner name.
+          4.2. Number of rooms.
+          4.3. Number of bathrooms.
+          4.4. If you have a garage.
+      5. The menu should also have the option to show all saved properties.
+
+🇧🇷: Escreva um programa em javascript que funcione como um cadastro de imóveis e atenda aos seguintes requisitos:
+
+    1. Deve ter um menu interativo que sempre é exibido até que o usuário escolha sair.
+    2. O menu interativo deve mostrar no topo a quantidade de imóveis cadastrados.
+    3. O menu deve ter a opção de salvar um imóvel.
+    4. Para salvar um novo imóvel o programa deve pedir as seguintes informações: Nome do proprietário.Quantidade de quartos.Quantidade de banheiros.Se possui garagem.
+        4.1. Nome do proprietário.
+        4.2. Quantidade de quartos.
+        4.3. Quantidade de banheiros.
+        4.4. Se possui garagem.
+    5. O menu também deve ter a opção de mostrar todos os imóveis salvos.
+
+*/
