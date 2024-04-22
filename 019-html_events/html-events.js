c206c4a667d3d177f1f721145ed5b339c1ac487a
Lucas Araújo <lucasaraujodev.contact@gmail.com>
2024-04-22 13:09:42 -0300

    html events
    tests and studies about html events included (#19)

---
+++ 019-html_events/html-events.js
@@ -0,0 +1,21 @@
+// ----- TESTS -----
+
+function onMouseEnter () {
+  console.log("Mouse Enter actived.")
+}
+
+function onChange () {
+  console.log("On Change actived.")
+}
+
+function onFocus () {
+  console.log("On Focus actived.")
+}
+
+function onBlur () {
+  console.log("On Blur actived.")
+}
+
+function onClick () {
+  console.log("On Click actived.")
+}
