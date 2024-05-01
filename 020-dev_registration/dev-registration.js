187701f74d9405e4f4057238e32ae2f9a5d9aa5e
Lucas Araújo <lucasaraujodev.contact@gmail.com>
2024-05-01 20:43:03 -0300

    exercise #20
    explanation included

---
+++ 020-dev_registration/dev-registration.js
@@ -0,0 +1,27 @@
+// ----- EXPLANATION OF THE EXERCISE -----
+
+/*
+
+🇺🇸: Build a web page that allows you to register developers, their technologies and experience.
+
+- The page must have an entry for the developer's full name and a button to add a new technology.
+- When clicking the button, an event must be triggered, creating a new line of inputs on the form containing:
+  - a text field for the name of the technology
+  - a group of “radio” buttons with experience time options (e.g. “0-2 years”, “3-4 years” and “5+ years”)
+  - a remove button that should delete that line.
+- The form must also have a register button that saves the dev's information in an array and clears the form. To test how it works, you can use a simple console.log()
+
+For this exercise you must only use events added via javascript and the page must work without triggering a reload.
+
+🇧🇷: Construa uma página web que permita cadastrar desenvolvedores, suas tecnologias e tempos de experiência. 
+
+- A página deve ter um input para o nome completo do desenvolvedor e um botão para adicionar uma nova tecnologia.
+- Ao clicar no botão um evento deve ser disparado criando no formulário uma nova linha de inputs contendo:
+    - um campo de texto para o nome da tecnologia
+    - um grupo de botões tipo “radio” com opções de tempo de experiência (por exemplo, “0-2 anos”, “3-4 anos” e “5+ anos”)
+    - um botão de remover que deve excluir essa linha.
+- O formulário também deve ter um botão de cadastrar que salva as informações do dev em um array e limpa o formulário. Para testar o funcionamento você pode utilizar um simples console.log()
+
+Para esse exercício você deve utilizar apenas eventos adicionados via javascript e a página deve funcionar sem acionar um recarregamento.
+
+*/
