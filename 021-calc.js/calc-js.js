c39db4b7794d1d2ec91822ffe04efbea9e8f8c95
Lucas Araújo <lucasaraujodev.contact@gmail.com>
2024-05-02 21:35:56 -0300

    exercise #21 and #22
    solutions added

---
+++ 021-calc.js/calc-js.js
@@ -0,0 +1,84 @@
+const main = document.querySelector('main')
+const root = document.querySelector(':root')
+const input = document.getElementById('input')
+const resultInput = document.getElementById('result')
+
+// impedindo que o user digite outros caracteres
+const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
+
+document.querySelectorAll('.charKey').forEach(function(charKeyBtn) {
+    charKeyBtn.addEventListener('click', function() {
+        const value = charKeyBtn.dataset.value
+        input.value += value
+    })
+})
+
+document.getElementById('clear').addEventListener('click', function() {
+    input.value = ''
+    input.focus()
+})
+
+// evento quando uma tecla é pressionada (keydown)
+input.addEventListener('keydown', function(ev) {
+    ev.preventDefault()
+    if (allowedKeys.includes(ev.key)) {
+        input.value += ev.key
+        return
+    }
+    //backspace
+    if (ev.key === 'Backspace') {
+        input.value = input.value.slice(0, -1)
+    }
+    //enter
+    if (ev.key === 'Enter') {
+        calculate()
+    }
+})
+
+document.getElementById('equal').addEventListener('click', calculate)
+
+function calculate() {
+// test console.log('Calculado!')
+// previndo erros
+    resultInput.value = 'ERROR'
+    resultInput.classList.add('error')
+    // avaliar e realizar calculo
+    const result = eval(input.value)
+    
+    resultInput.value = result
+    resultInput.classList.remove('error')
+}
+
+// copy to clipboard
+document.getElementById('copyToClipboard').addEventListener('click', function(ev) {
+    const button = ev.currentTarget
+    if(button.innerText === 'Copy') {
+        button.innerText = 'Copied!'
+        button.classList.add('success')
+        //propriedad de cópia (propriedades de navigator - clipboard)
+        window.navigator.clipboard.writeText(resultInput.value)
+    } else {
+        button.innerText = 'Copy'
+        button.classList.remove('success')
+    }
+})
+
+//switch the theme (baseado nas variáveis do CSS)
+
+document.getElementById('themeSwitcher').addEventListener('click', function() {
+    if (main.dataset.theme === 'dark') {
+        root.style.setProperty('--bg-color', '#f1f5f9')
+        root.style.setProperty('--border-color', '#aaa')
+        root.style.setProperty('--font-color', '#212529')
+        root.style.setProperty('--primary-color', '#26834a')
+        main.dataset.theme = 'light'
+    } else {
+        root.style.setProperty('--bg-color', '#212529')
+        root.style.setProperty('--border-color', '#666')
+        root.style.setProperty('--font-color', '#fif5f9')
+        root.style.setProperty('--primary-color',   '#4dff91')
+        main.dataset.theme = 'dark'
+    }
+})
+
+
---
+++ 022-tic_tac_toe/tic-tac-toe.js
@@ -0,0 +1,119 @@
+// ----- EXPLANATION OF THE EXERCISE -----
+
+/*
+
+🇺🇸: Build a tic-tac-toe game, known in Portuguese as tic-tac-toe, using HTML, CSS and Javascript. You can develop freely, however you prefer, but the game must meet the following requirements:
+
+- It must be possible to include the names of both players;
+- The name of the current player must be shown on the screen and changed as the turns alternate;
+- A board must be shown on the screen and updated when the player clicks on the region he wants to mark;
+- When a player clicks on the board, an “X” or “O” must be marked according to the current player and it must not be possible to click on that region again;
+- When a player wins their name must be shown on the screen and the victory regions must be highlighted in some way;
+- In case of a tie, a tie message must be shown on the screen;
+- It should be possible to restart the game to play again.
+
+🇧🇷: Construa um jogo tic-tac-toe, conhecido em português como jogo da velha, utilizando HTML, CSS e Javascript. Você pode desenvolver com liberdade, da forma que preferir, mas o jogo precisa atender aos seguintes requisitos:
+
+- Deve ser possível incluir os nomes dos dois jogadores;
+- O nome do jogador da vez deve ser mostrado na tela e alterado a medida que os turnos vão se alternando;
+- Um tabuleiro deve ser mostrado na tela e ser atualizado quando o jogador clicar na região que ele quer marcar;
+- Quando um jogador clicar no tabuleiro deve ser marcado um “X” ou “O” de acordo com o jogador da vez e não deve ser possível clicar naquela região novamente;
+- Quando um jogador ganhar seu nome deve ser mostrado na tela e as regiões da vitória devem ser destacadas de alguma forma;
+- Em caso de empate, uma mensagem de empate deve ser mostrada na tela;
+- Deve ser possível reiniciar o jogo para jogar novamente.
+
+*/
+
+// Variáveis globais úteis
+const boardRegions = document.querySelectorAll('#gameBoard span')
+let vBoard = []
+let turnPlayer = ''
+
+function updateTitle() {
+  const playerInput = document.getElementById(turnPlayer)
+  document.getElementById('turnPlayer').innerText = playerInput.value
+}
+
+function initializeGame() {
+  // Inicializa as variáveis globais 
+  vBoard = [['', '', ''], ['', '', ''], ['', '', '']]
+  turnPlayer = 'player1'
+  // Ajusta o título da página (caso seja necessário)
+  document.querySelector('h2').innerHTML = 'Vez de: <span id="turnPlayer"></span>'
+  updateTitle()
+  // Limpa o tabuleiro (caso seja necessário) e adiciona os eventos de clique
+  boardRegions.forEach(function (element) {
+    element.classList.remove('win')
+    element.innerText = ''
+    element.classList.add('cursor-pointer')
+    element.addEventListener('click', handleBoardClick)
+  })
+}
+// Verifica se existem três regiões iguais em sequência e devolve as regiões
+function getWinRegions() {
+  const winRegions = []
+  if (vBoard[0][0] && vBoard[0][0] === vBoard[0][1] && vBoard[0][0] === vBoard[0][2])
+    winRegions.push("0.0", "0.1", "0.2")
+  if (vBoard[1][0] && vBoard[1][0] === vBoard[1][1] && vBoard[1][0] === vBoard[1][2])
+    winRegions.push("1.0", "1.1", "1.2")
+  if (vBoard[2][0] && vBoard[2][0] === vBoard[2][1] && vBoard[2][0] === vBoard[2][2])
+    winRegions.push("2.0", "2.1", "2.2")
+  if (vBoard[0][0] && vBoard[0][0] === vBoard[1][0] && vBoard[0][0] === vBoard[2][0])
+    winRegions.push("0.0", "1.0", "2.0")
+  if (vBoard[0][1] && vBoard[0][1] === vBoard[1][1] && vBoard[0][1] === vBoard[2][1])
+    winRegions.push("0.1", "1.1", "2.1")
+  if (vBoard[0][2] && vBoard[0][2] === vBoard[1][2] && vBoard[0][2] === vBoard[2][2])
+    winRegions.push("0.2", "1.2", "2.2")
+  if (vBoard[0][0] && vBoard[0][0] === vBoard[1][1] && vBoard[0][0] === vBoard[2][2])
+    winRegions.push("0.0", "1.1", "2.2")
+  if (vBoard[0][2] && vBoard[0][2] === vBoard[1][1] && vBoard[0][2] === vBoard[2][0])
+    winRegions.push("0.2", "1.1", "2.0")
+  return winRegions
+}
+// Desabilita uma região do tabuleiro para que não seja mais clicável
+function disableRegion(element) {
+  element.classList.remove('cursor-pointer')
+  element.removeEventListener('click', handleBoardClick)
+}
+// Pinta as regiões onde o jogador venceu e mostra seu nome na tela
+function handleWin(regions) {
+  regions.forEach(function (region) {
+    document.querySelector('[data-region="' + region + '"]').classList.add('win')
+  })
+  const playerName = document.getElementById(turnPlayer).value
+  document.querySelector('h2').innerHTML = playerName + ' venceu!'
+}
+
+function handleBoardClick(ev) {
+  // Obtém os índices da região clicada
+  const span = ev.currentTarget
+  const region = span.dataset.region // N.N
+  const rowColumnPair = region.split('.') // ["N", "N"]
+  const row = rowColumnPair[0]
+  const column = rowColumnPair[1]
+  // Marca a região clicada com o símbolo do jogador
+  if (turnPlayer === 'player1') {
+    span.innerText = 'X'
+    vBoard[row][column] = 'X'
+  } else {
+    span.innerText = 'O'
+    vBoard[row][column] = 'O'
+  }
+  // Limpa o console e exibe nosso tabuleiro virtual
+  console.clear()
+  console.table(vBoard)
+  // Desabilita a região clicada
+  disableRegion(span)
+  // Verifica se alguém venceu
+  const winRegions = getWinRegions()
+  if (winRegions.length > 0) {
+    handleWin(winRegions)
+  } else if (vBoard.flat().includes('')) {
+    turnPlayer = turnPlayer === 'player1' ? 'player2' : 'player1'
+    updateTitle()
+  } else {
+    document.querySelector('h2').innerHTML = 'Empate!'
+  }
+}
+// Adiciona o evento no botão que inicia o jogo
+document.getElementById('start').addEventListener('click', initializeGame)
